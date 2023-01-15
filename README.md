60760e5bc944ee571bfb8b43504cbdcc6f005353
Accountant Foreign <114130506+Accountant-foreign@users.noreply.github.com>
2023-01-15 14:14:40 +0400

    Spacing and demo link to readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tic-tac-toe
+
+Live demo: https://Accountant-Foreign.github.io/tic-tac-toe
--- script.js
+++ script.js
@@ -24,16 +24,22 @@ const counter = counterCreator();
 let createBoard=(function(){
     let cells=document.getElementsByClassName('cell');
     let cellMarker=document.createElement('div');
+    let restartButton=document.getElementById('restart');
+
     for(let i=0; i<9; i++){
         let clonedCellMarker=cellMarker.cloneNode(true);
         clonedCellMarker.className=`${gameBoard[i]}`;
         cells[i].append(clonedCellMarker);
     }
+
+    restartButton.setAttribute('onclick', 'location.reload()');
 })();
 
 let createPlayers=(function () {
+
     let playerX=player('one', 'X');
     let playerY=player('two', 'O');
+
     return {playerX, playerY};
 })();
 
@@ -41,17 +47,21 @@ let markDown=(function(){
     let cells=Array.from(document.getElementsByClassName('cell'));
     let span=document.getElementById('getClass');
     let currentPlayer="";
+
     cells.forEach(cell => {
         cell.addEventListener('mousedown', ()=>{
             let claSS=Number(cell.firstChild.className);
+
             if(Number.isInteger(claSS)) {
                 let playerTurn=counter();
+
                 if(playerTurn%2==1){
                     cell.firstChild.className=createPlayers.playerX.marker;
                     gameBoard[claSS-1]=createPlayers.playerX.marker;
                     span.textContent=createPlayers.playerY.marker;
                     currentPlayer=createPlayers.playerX.marker;
-                } else {
+                } 
+                else {
                     cell.firstChild.className=createPlayers.playerY.marker;
                     gameBoard[claSS-1]=createPlayers.playerY.marker;
                     span.textContent=createPlayers.playerX.marker;
@@ -61,7 +71,9 @@ let markDown=(function(){
         });
 
         cell.addEventListener('mouseup', ()=>{
+
             const isStringsArray =gameBoard.every(i => typeof i === "string")
+
             if(gameBoard[0]==gameBoard[1]&&gameBoard[1]==gameBoard[2]||gameBoard[3]==gameBoard[4]&&gameBoard[4]==gameBoard[5]||gameBoard[6]==gameBoard[7]&&gameBoard[7]==gameBoard[8]){
                 span.parentElement.innerHTML="Player "+currentPlayer+" wins!";
                 document.body.innerHTML=document.body.innerHTML;
@@ -77,7 +89,8 @@ let markDown=(function(){
             if(isStringsArray){
                 try {
                     span.parentElement.innerHTML="The game is a tie...";
-                } catch (error) {
+                } 
+                catch (error) {
                     
                 }
                 document.body.innerHTML=document.body.innerHTML;
@@ -85,9 +98,3 @@ let markDown=(function(){
         })
     });
 })();
-
-let restart=(function(){
-    let restartButton=document.getElementById('restart');
-    restartButton.setAttribute('onclick', 'location.reload()');
-})();
-
